b144066e26abc0f0239ae45bc4d5e74cfa6c1c6a
HarshYadav152 <harshyadav152@outlook.com>
2025-12-16 21:43:36 +0530

    config for mongodb database

---
+++ gsecure/lib/db/mongodb.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectingtoDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/gsecure}`)
+        console.log(`MongoDB connected At HOST :: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Failed to connect to MONGODB : ",error);
+        process.exit(1);
+    }
+}
+
+export default connectingtoDB;
