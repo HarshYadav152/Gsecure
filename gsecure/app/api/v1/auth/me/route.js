2f9691f7fddff922ee2875b949d13b5e00df83e8
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2026-01-19 15:42:30 +0530

    get user route for fetching loggedinuser

---
+++ gsecure/app/api/v1/auth/me/route.js
@@ -0,0 +1,42 @@
+import connectingtoDB from '@/lib/db/mongodb';
+import { User } from '@/lib/models/User.js';
+import { verifyAccessToken } from '@/lib/utils/jwt';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET(req) {
+  try {
+    await connectingtoDB();
+    const token = await cookies.get("authToken")?.value;
+    if (!token) {
+      return NextResponse.json(
+        { success: false, message: "Unauthorized - No token provided" },
+        { status: 401 }
+      );
+    }
+
+    // Verify token and get user
+    const decoded = await verifyAccessToken(token);
+    
+    const user = await User.findById(decoded.userId).select('-password');
+
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, data: user, message: "User fetched successfully." },
+      { status: 200 }
+    );
+
+  } catch (error) {
+    console.error('Get current user error:', error);
+    return NextResponse.json(
+      { success: false, message: "Unauthorized" },
+      { status: 401 }
+    );
+  }
+}
