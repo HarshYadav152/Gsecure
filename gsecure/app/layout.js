8976f5001ee9845d4c3742e3a774d612e9b1dfa5
Harsh Yadav <161936085+HarshYadav152@users.noreply.github.com>
2026-01-19 15:47:13 +0530

    add authProvider and Navbar and Footer in layout.

--- gsecure/app/layout.js
+++ gsecure/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from "@/lib/contexts/AuthContext";
+import Nabvar from "@/components/ui/common/Navbar";
+import Footer from "@/components/ui/common/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +25,15 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="fixed inset-0 z-[-2] h-screen w-screen bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
+
+        <AuthProvider>
+          <Nabvar />
+          <main className="relative z-0 min-h-[calc(100vh-120px)]">
+            {children}
+          </main>
+          <Footer/>
+        </AuthProvider>
       </body>
     </html>
   );
